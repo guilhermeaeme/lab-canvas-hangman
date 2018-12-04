b6e3fe31c0f1f80d4961b80525facbe8fc2ec626
guilhermeaeme <guilhermeaeme@gmail.com>
2018-12-03 22:38:19 -0200

    jasmine

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,37 +1,43 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+	this.words = ['IRONHACK', 'NODEJS', 'JAVASCRIPT', 'METEOR', 'ANGULAR', 'BARCELONA', 'MADRID', 'MIAMI', 'HTML'],
+	this.secretWord = '',
+	this.letters = [],
+	this.guessedLetter = '',
+	this.errorsLeft = 10
+}
 
-// }
+Hangman.prototype.getWord = function () {
+	this.secretWord = this.words[Math.floor(Math.random() * this.words.length)]
+	return this.secretWord;
+};
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.checkIfLetter = function (keyCode) {
+	return (keyCode >= 65 && keyCode <= 90);
+};
 
-// };
+Hangman.prototype.checkClickedLetters = function (key) {
+	return (this.letters.indexOf(key.toUpperCase()) == -1);
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.addCorrectLetter = function (i) {
+	this.guessedLetter += this.secretWord[i].toUpperCase();
+};
 
-// };
+Hangman.prototype.addWrongLetter = function (letter) {
+	this.errorsLeft--;
+};
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+Hangman.prototype.checkGameOver = function () {
+	return this.errorsLeft == 0;
+};
 
-// };
+Hangman.prototype.checkWinner = function () {
+	return this.guessedLetter.length == this.secretWord.length;
+};
 
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
-
+/*
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
 };
@@ -40,3 +46,4 @@ document.getElementById('start-game-button').onclick = function () {
 document.onkeydown = function (e) {
 
 };
+*/
